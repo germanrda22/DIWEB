5789a472db0b54fd2b05f325c4dae6be2b62f984
German Ruiz <germanrdarco@gmail.com>
2022-01-19 21:50:08 +0100

    18/01/2022

--- Segundo_trimestre/Portofolio/app.js
+++ Segundo_trimestre/Portofolio/app.js
@@ -1,10 +1,10 @@
-const hamburgerContainer = document.querySelector("#header__main-nav");
-const hamburger = document.querySelector(".header__main-nav--hamburger");
-const links = document.querySelectorAll(".header__main-nav--links li");
+const hamburgerContainer = document.querySelector('#nav_cabecera_principal');
+const hamburger = document.querySelector('.nav_cabecera_espacios');
+const links = document.querySelectorAll('.nav_cabecera_principal_links li');
 
-hamburger.addEventListener("click", () => {
-  hamburgerContainer.classList.toggle("clicked");
-//  links.forEach((link) => {
-//    link.classList.toggle("fade");
-//  });
+hamburger.addEventListener('click', () => {
+  hamburgerContainer.classList.toggle('clicked');
+  links.forEach((link) =>{
+    link.classList.toggle('fade');
+  });
 });
